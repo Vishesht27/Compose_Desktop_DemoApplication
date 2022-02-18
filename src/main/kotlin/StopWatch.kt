b0401afe175dbb4c13715f1376988afdf80f9b9c
Vishesh <vishesht27@gmail.com>
2022-02-18 11:31:32 +0530

    Added formatTime,Reset & Pause functions

--- src/main/kotlin/StopWatch.kt
+++ src/main/kotlin/StopWatch.kt
@@ -1,9 +1,12 @@
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class StopWatch {
 
@@ -20,7 +23,37 @@ class StopWatch {
 
         coroutineScope.launch {
             lastTimestamp = System.currentTimeMillis()
-            isActive = true
+            this@StopWatch.isActive = true
+            while(isActive) {
+                delay(10L)
+                timeMillis += System.currentTimeMillis() - lastTimestamp
+                lastTimestamp = System.currentTimeMillis()
+            }
         }
+    }
+
+    fun pause(){
+        isActive = false
+    }
+
+    fun reset(){
+        coroutineScope.cancel()
+        coroutineScope = CoroutineScope(Dispatchers.Main)
+        timeMillis = 0L
+        lastTimestamp = 0L
+        formattedTime = "00:00:000"
+        isActive = false
+    }
+
+    private fun formatTime(timeMillis: Long): String {
+        val localDateTime = LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(timeMillis),
+            ZoneId.systemDefault()
+        )
+        val formatter = DateTimeFormatter.ofPattern(
+            "mm:ss:SSS",
+            Locale.getDefault()
+        )
+        return localDateTime.format(formatter)
     }
 }
